46bad9eee52eec29a8b265f9b78c666a1ff00a90
pacotreflip <froker91@gmail.com>
2018-07-17 10:08:09 -0500

    [REPOSITORY] : Crear Interfaz Y Repositorio e implementar funciones

---
+++ app/Repositories/Repository.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Repositories;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Repository implements RepositoryInterface
+{
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * Repository constructor.
+     * @param Model $model
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+
+    public function all(array $columns = ['*'])
+    {
+        return $this->model->get($columns);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function destroy($id)
+    {
+        return $this->model->destroy($id);
+    }
+
+    public function find($id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function orderBy($column, $direction = 'ASC')
+    {
+        $this->model = $this->model->orderBy($column, $direction);
+    }
+
+    public function update(array $data, $id)
+    {
+        $recod = $this->find($id);
+        return $recod->update($data);
+    }
+}
---
+++ app/Repositories/RepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+
+interface RepositoryInterface
+{
+    public function all(array $columns = ['*']);
+    public function create(array $data);
+    public function destroy($id);
+    public function find($id);
+    public function orderBy($column, $direction = 'ASC');
+    public function update(array $data, $id);
+}
